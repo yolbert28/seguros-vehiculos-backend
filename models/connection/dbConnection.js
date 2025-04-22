a16e60e788e591e5a1768637a37a79d9b228438e
yolbert28 <yolbertdev@gmail.com>
2025-04-22 19:29:13 -0400

    cero variables de entorno

--- models/connection/dbConnection.js
+++ models/connection/dbConnection.js
@@ -1,6 +1,6 @@
 import { createClient } from "@libsql/client";
 
 export const turso = createClient({
-  url: process.env.TURSO_DATABASE_URL,
-  authToken: process.env.TURSO_AUTH_TOKEN,
+  url: "libsql://vehicle-insurance-yolbert28.turso.io",
+  authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NDIwNDY2NzcsImlkIjoiM2I5YjUwMzUtMzFhZC00ZDdmLTg5MDAtOWVmMDZlYTgxNzMxIn0.kE72PyjobCFI1QL6BewEyyQ__yRtWRllCPlAbuNMDgr6pzwMcPrMKPhzk82eQgaDk1BO3p-jRCb8uIxh9WMtAA",
 });
